a9511fd4e3eeb93df18fd7132fd9b247be01061d
Howsco <1120132909@qq.com>
2025-03-14 23:25:46 +0800

    事件绑定与event事件对象

--- 01-mpbase/pages/index/index.js
+++ 01-mpbase/pages/index/index.js
@@ -1,2 +1,12 @@
 // index.js
-Page({})
+Page({
+
+  // 事件处理函数需要写到index.js中的Page()方法中
+  handler() {
+    console.log('事件被触发了。。。')
+  },
+
+  getInputVal(event) {
+    console.log(event.detail.value)
+  }
+})
