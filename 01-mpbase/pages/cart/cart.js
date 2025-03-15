6e71ba7c5abea6601747d13af744a16cad462f45
Howsco <1120132909@qq.com>
2025-03-15 18:00:22 +0800

    使用双大括号绑定数据

--- 01-mpbase/pages/cart/cart.js
+++ 01-mpbase/pages/cart/cart.js
@@ -10,6 +10,15 @@ Page({
     // 点击按钮时，事件对象上的mark获取的是 事件触发节点 和其父节点 身上所有的mark数据
     // 点击蓝色区域时，事件对象上的mark获取的是 view 身上绑定的数据
     console.log(event)
+  },
+
+  data: {
+    name: '张三',
+    age: 18,
+    obj: {
+      num: 2
+    },
+    id: 1
   }
 })
 
