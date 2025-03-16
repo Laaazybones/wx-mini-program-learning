faebb49b097a5934ebccfa081bb2020c63af6b79
Howsco <1120132909@qq.com>
2025-03-16 23:54:05 +0800

    发起网络请求 wx.request()

--- 05-API/pages/index/index.js
+++ 05-API/pages/index/index.js
@@ -1,2 +1,30 @@
 // index.js
-Page({})
+Page({
+  data: {
+    list: []
+  },
+
+  getData() {
+    wx.request({
+      url: 'https://gmall-prod.atguigu.cn/mall-api/index/findBanner',
+      method: 'GET',
+      data: {}, // 请求参数
+      header: {},
+      // API调用成功后进行的回调
+      success: (res) => {
+        this.setData({
+          list: res.data.data
+        })
+        console.log(res)
+      },
+      // API调用失败后进行的回调
+      fail: (res) => {
+        console.log(res)
+      },
+      // API不管调用成功还是失败，都进行调用的回调
+      complete: (res) => {
+        console.log(res)
+      }
+    })
+  }
+})
