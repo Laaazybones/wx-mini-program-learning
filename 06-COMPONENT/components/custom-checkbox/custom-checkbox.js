c5492422ed65ac54833e689ca6569dc57d3fe84f
Howsco <1120132909@qq.com>
2025-03-17 23:09:02 +0800

    自定义组件的数据和方法书写

--- 06-COMPONENT/components/custom-checkbox/custom-checkbox.js
+++ 06-COMPONENT/components/custom-checkbox/custom-checkbox.js
@@ -12,13 +12,17 @@ Component({
    * 组件的初始数据
    */
   data: {
-
+    isChecked: false
   },
 
   /**
    * 组件的方法列表
    */
   methods: {
-
+    updateChecked() {
+      this.setData({
+        isChecked: !this.data.isChecked
+      })
+    }
   }
 })
