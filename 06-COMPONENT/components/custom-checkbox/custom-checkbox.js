d78b067a9de99be08de4b5c0bd1344491fe5bd7a
Howsco <1120132909@qq.com>
2025-03-18 21:46:57 +0800

    修改默认checkbox样式，并实现样式穿透

--- 06-COMPONENT/components/custom-checkbox/custom-checkbox.js
+++ 06-COMPONENT/components/custom-checkbox/custom-checkbox.js
@@ -1,6 +1,10 @@
 // components/custom-checkbox/custom-checkbox.js
 Component({
 
+  options: {
+    styleIsolation: "shared"
+  },
+
   /**
    * 组件的属性列表：组件对外接收的属性，主要用来接收组件使用者传递给组件内部的数据和内容
    */
@@ -38,7 +42,7 @@ Component({
     updateChecked() {
       this.setData({
         isChecked: !this.data.isChecked,
-        label: '在组件内部也可以修改传入的数据'
+        // label: '在组件内部也可以修改传入的数据'
       })
 
       // 在 JS 中可以访问和获取 properties 中的数据，并且还能对他进行修改，但是一般不建议这么操作，因为会造成数据流的混乱
