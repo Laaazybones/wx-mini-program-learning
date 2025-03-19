8013a1d112d9cac11d100555c184795be39de00e
Howsco <1120132909@qq.com>
2025-03-19 16:48:55 +0800

    小程序应用、页面、组件生命周期总结

---
+++ 06-COMPONENT/components/custom07/custom07.js
@@ -0,0 +1,42 @@
+// components/custom07/custom07.js
+Component({
+
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  },
+
+  lifetimes: {
+    created() {
+      console.log('★★★小程序组件 - created')
+    },
+
+    attached() {
+      console.log('★★★小程序组件 - attached')
+    },
+
+    ready() {
+      console.log('★★★小程序组件 - ready')
+    },
+
+    detached() {
+      console.log('★★★小程序组件 - detached')
+    }
+  }
+})
--- 06-COMPONENT/pages/list/list.js
+++ 06-COMPONENT/pages/list/list.js
@@ -12,55 +12,55 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad(options) {
-
+    console.log('★★小程序页面 - list - onLoad')
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady() {
-
+    console.log('★★小程序页面 - list - onReady')
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
   onShow() {
-
+    console.log('★★小程序页面 - list - onShow')
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
   onHide() {
-
+    console.log('★★小程序页面 - list - onHide')
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
   onUnload() {
-
+    console.log('★★小程序页面 - list - onUnload')
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
   onPullDownRefresh() {
-
+    console.log('★★小程序页面 - list - onPullDownRefresh')
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
   onReachBottom() {
-
+    console.log('★★小程序页面 - list - onReachBottom')
   },
 
   /**
    * 用户点击右上角分享
    */
   onShareAppMessage() {
-
+    console.log('★★小程序页面 - list - onShareAppMessage')
   }
 })
