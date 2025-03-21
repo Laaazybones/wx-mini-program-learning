1e5d45918429723446eef8a32ce3d4858cd6eae0
Howsco <1120132909@qq.com>
2025-03-21 16:03:35 +0800

    使用getApp()获取App的实例对象，进而获取app.js中定义的数据和方法

--- 09-OPENABILITY/miniprogram/app.js
+++ 09-OPENABILITY/miniprogram/app.js
@@ -1,2 +1,16 @@
 // app.js
-App({})
+App({
+  // 在 App.js 中 可以通过 this 来获得 APP 对象实例
+  // 不能通过 getApp() 函数来获取 APP 对象实例
+
+  // 全局共享的数据
+  globalData: {
+    token: ''
+  },
+
+  // 全局共享的方法
+  setToken(token) {
+    this.globalData.token = token
+  }
+
+})
