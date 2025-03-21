d29131f868306d96da80cdd74ab99bd252d977c8
Howsco <1120132909@qq.com>
2025-03-21 15:30:05 +0800

    开放能力 - 获取用户手机号

--- 09-OPENABILITY/miniprogram/pages/list/list.js
+++ 09-OPENABILITY/miniprogram/pages/list/list.js
@@ -1,66 +1,15 @@
-// pages/list/list.js
 Page({
-
-  /**
-   * 页面的初始数据
-   */
-  data: {
-
+  // 快速验证手机
+  getPhoneNumber(event) {
+    // 通过事件对象，可以在 event.detail 中获取到 code 属性
+    // code：动态令牌，可以使用 code 获取用户的手机号
+    // 需要将 code 发送给后端，后端在接收到 code 后，调用API，得到用户真实手机号
+    // 在换取手机号成功后，后端会将手机号返回给前端
+    console.log(event)
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad(options) {
-
+  
+  // 实时验证手机
+  getRealtimePhoneNumber(event) {
+    console.log(event)
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady() {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow() {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide() {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload() {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh() {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom() {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage() {
-
-  }
 })
