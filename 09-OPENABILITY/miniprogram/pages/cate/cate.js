3f218f0e88f5f21e3a4c501f3863f0e2afbe993a
Howsco <1120132909@qq.com>
2025-03-21 10:26:15 +0800

    开放能力-转发: onShareAppMessage(), open-type='share'

--- 09-OPENABILITY/miniprogram/pages/cate/cate.js
+++ 09-OPENABILITY/miniprogram/pages/cate/cate.js
@@ -1,66 +1,11 @@
-// pages/cate/cate.js
 Page({
-
-  /**
-   * 页面的初始数据
-   */
-  data: {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad(options) {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady() {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow() {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide() {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload() {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh() {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom() {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage() {
-
+  // 监听页面按钮的转发 以及 右上角的转发按钮
+  onShareAppMessage(obj) {
+    console.log(obj)
+    return {
+      title: '这是一个神奇的网站！',
+      path: 'pages/cate/cate',
+      imageUrl: '../../assets/category/cate.png'
+    }
   }
 })
